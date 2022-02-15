6f05a0c1bb6e37b011f435cd6cf7edb42eb977f9
Rohit <rohit.gupta@neosoftmail.com>
2022-02-15 16:01:29 +0530

    added content to navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,12 @@
+import reactLogo from "../images/react-icon3.png";
+
 function Navbar() {
     return (
-        <h1>This is navbar</h1>
+        <nav className="navbar">
+            <img src={reactLogo} alt="react logo" className="react-logo" />
+            <h3 className="logo-text">React Facts</h3>
+            <h4 className="nav-title">Learning React</h4>
+        </nav>
     );
 }
 
