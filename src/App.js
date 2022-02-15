5198f9d5309c68e504544c03830fa1097a33e440
Rohit <rohit.gupta@neosoftmail.com>
2022-02-15 10:50:00 +0530

    wrote the content of first project

--- src/App.js
+++ src/App.js
@@ -1,10 +1,22 @@
 import React, {Component} from "react";
 
-var unorderedList = <ul><li>this is list item 1.</li><li>this is list item 2.</li></ul>
+var pageContent = (
+  <div>
+    <img src="../public/react-logo.png" width="40px" alt="react logo"/>
+    <h1>Fun facts about React</h1>
+    <ul>
+      <li>Was first released in 2013</li>
+      <li>Was originally created by Jordan Walke</li>
+      <li>Has well over 100k stars on GitHub</li>
+      <li>Is maintained by Facebook</li>
+      <li>Powers thousands of enterprise apps, including mobile apps</li>
+    </ul>
+  </div>
+)
 
 class App extends Component {
     render() {
-      return unorderedList;
+      return pageContent;
     }
 }
 
